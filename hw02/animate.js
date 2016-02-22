b7fcd954b397ebb59b6d6df0aa5652874b7272f4
Johnny So <jso123450@gmail.com>
2016-02-22 12:51:56 -0500

    Done

--- hw02/animate.js
+++ hw02/animate.js
@@ -5,7 +5,7 @@ console.log("Ready for some dotconn'ing!");
 
 /*--------------- HEADING ---------------*/
 var hw = document.getElementById("hw");
-hw.innerHTML = "HW2 -- Animate!";
+hw.innerHTML = "HW02 -- Animate!";
 
 var date = document.getElementById("date");
 date.innerHTML = "2016-02-22";
@@ -23,18 +23,20 @@ var width = 538;
 var radius = 0;
 var ballooning = true;
 
-var clear = function(){
-    ctx.clearRect(0,0,width,height);
-};
-
 var drawCircle = function(){
-    clear;
-    if (ballooning){
+    ctx.clearRect(0,0,width,height);
+    if (radius == 0)
+	ballooning = true;
+    else if (radius == width/2)
+	ballooning = false;
+    if (ballooning)
 	radius = radius+1;
-    }
-    else {
+    else
 	radius = radius-1;
-    }
+    ctx.beginPath();
+    ctx.arc(width/2,height/2,radius,0,6.28);
+    ctx.closePath();
+    ctx.fill();
     window.requestAnimationFrame( drawCircle );
 };
 
