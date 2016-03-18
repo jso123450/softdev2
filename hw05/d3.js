5377e62fb8546852f38d2e089bc31c60d589ccfb
Johnny So <jso123450@gmail.com>
2016-03-18 10:16:54 -0400

    stuff from yesterday

--- hw05/d3.js
+++ hw05/d3.js
@@ -2,7 +2,7 @@
 console.log("Cats!");
 
 /* -------------- D3 -------------- */
-//d3.selectAll("p").style("color","white");
-//d3.select("h1").style("color","white");
-//d3.select("h2").style("color","white");
-//d3.select("body").style("background-color","black");
+d3.selectAll("p").style("color","white");
+d3.select("h1").style("color","white");
+d3.select("h2").style("color","white");
+d3.select("body").style("background-color","black");
